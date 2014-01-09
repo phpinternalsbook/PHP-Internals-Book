586da9c2dffa9d57d192cbe753ebfb89efaf0b6a
nikic <nikita.ppv@googlemail.com>
2014-01-09 16:39:56 +0100

    Add extra script for a full rebuild
    Includes code to remove old build files - that way we don't keep
    outdated files in the git repo.

---
+++ build_release_html.sh
@@ -0,0 +1,7 @@
+# get rid of old files, so we don't keep them around in the git repo
+# when a file or directory was renamed
+rm -rf BookHTML/html/*/
+rm -f BookHTML/html/*.html
+rm -f BookHTML/html/.buildinfo
+
+sphinx-build -b html -d BookHTML/doctrees -a Book BookHTML/html
